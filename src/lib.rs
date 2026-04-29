40f561c0562d2a4855a561182c9fe6093485f62d
Ryan Matimu <matimu.ryan@gmail.com>
2026-04-28 21:13:08 -0400

    Updated scan error function to include character and text line

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,22 @@
 pub mod lox;
+pub mod ast_printer;
 use std::process;
 use crate::lox::Token;
 use crate::lox::TokenType;
+use crate::lox::scanner;
+use crate::lox::parser;
 
 
 #[derive (Debug, PartialEq)]
-struct ParserError {
+pub struct ParserError {
     error_msg: String,
     error_token: Token,
 }
 
 
 
-pub fn error (line:usize, message: &str) {
-    report(line, "", message);
+pub fn scan_error (line:usize, ch:String, message: &str) {
+    report(line, format!("at '{}'", ch).as_str(), message);
     process::exit(1)
 }
 
